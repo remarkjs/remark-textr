76896aed9558bf2284aff02c6260c8649cd7d438
Denys Dovhan <email@denysdovhan.com>
2015-11-27 22:13:14 +0200

    Rewrite index.js.

--- index.js
+++ index.js
@@ -1,31 +1,12 @@
 import visit from 'unist-util-visit';
 import textr from 'textr';
 
-import TRANSFORMERS from './transformers';
-
 export default function attacher(
-  processor, {
-    locale = 'en-us',
-    before = [],
-    modules = {},
-    after = []
-  } = {}
+  processor, { plugins = [], ...options } = {}
 ) {
-  const tfs =
-    modules === false
-    ? []
-    : Object.keys(TRANSFORMERS)
-        .filter(item => modules[item] !== false && true)
-        .map(item => require(TRANSFORMERS[item]));
-
   return function transformer(ast) {
     visit(ast, 'text', node => {
-      node.value = (
-        textr({ locale })
-          .use(...before)
-          .use(...tfs)
-          .use(...after)
-      )(node.value);
+      node.value = (textr(options).use(...plugins))(node.value);
     });
   };
 }
