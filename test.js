8780927266c69990831414eb732822fb74ce88ee
Denys Dovhan <email@denysdovhan.com>
2015-12-26 01:32:45 +0200

    Revert "mdast-textr → remark-textr"

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 import { equal } from 'assert';
-import remark from 'remark';
-import remarkTextr from './index';
+import mdast from 'mdast';
+import mdastTextr from './index';
 
 // textr plugin — just function to replace triple dots to ellipses
 const ellipses = input => input.replace(/\.{3}/gim, '…');
@@ -11,16 +11,16 @@ const text = `
     function(...args) { return args; }
 `;
 
-it('should remarkTextr in node', () =>
+it('should mdastTextr in node', () =>
   equal(
-    remark.use(remarkTextr, { plugins: [ ellipses ] }).process(text),
+    mdast.use(mdastTextr, { plugins: [ ellipses ] }).process(text),
 `## spread operator…
 
     function(...args) { return args; }
 `));
 
-it('should remarkTextr in CLI (with options)', () =>
-  equal(remark.use(remarkTextr, {
+it('should mdastTextr in CLI (with options)', () =>
+  equal(mdast.use(mdastTextr, {
     plugins: [ 'typographic-ellipses', 'typographic-quotes' ],
     options: { locale: 'ru' }
   }).process('yo "there" ...\n'), 'yo «there» …\n')
