b820af946d56308860196275b4352624ce2e49ae
Titus Wormer <tituswormer@gmail.com>
2023-09-21 11:13:49 +0200

    Refactor to move code to `lib/`

--- index.js
+++ index.js
@@ -1,49 +1,6 @@
 /**
- * @typedef {import('mdast').Root} Root
- *
- * @callback TextrPlugin
- *   A textr plugin.
- * @param {string} value
- *   Value to transform.
- * @param {object} [options]
- *   Global configuration passed to textr.
- * @returns {string|void}
- *   Changed text (optional).
- *
- * @typedef Options
- *   Configuration.
- * @property {Array.<string|TextrPlugin>} [plugins]
- *   Textr plugins.
- * @property {object} [options]
- *   Configuration passed to `textr`.
+ * @typedef {import('./lib/index.js').Options} Options
+ * @typedef {import('./lib/index.js').TextrPlugin} TextrPlugin
  */
 
-import {visit} from 'unist-util-visit'
-import textr from 'textr'
-
-/**
- * Plugin to improve typography with Textr.
- *
- * @type {import('unified').Plugin<[Options?]|void[], Root>}
- */
-export default function remarkTextr(options = {}) {
-  const plugins = options.plugins || []
-  const promise = Promise.all(
-    plugins.map(async (fn) => {
-      if (typeof fn === 'string') {
-        /** @type {{default: TextrPlugin}} */
-        const mod = await import(fn)
-        return mod.default
-      }
-
-      return fn
-    })
-  ).then((list) => textr(options.options || {}).use(...list))
-
-  return async (tree) => {
-    const typography = await promise
-    visit(tree, 'text', (node) => {
-      node.value = typography.exec(node.value)
-    })
-  }
-}
+export {default} from './lib/index.js'
